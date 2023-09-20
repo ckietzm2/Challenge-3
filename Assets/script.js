c1389957766f69685d1634557631f49a241079a9
Cody K <ckietzm2@gmail.com>
2023-09-20 16:29:08 -0500

    Cleaned up code and got rid of lots of variables for simplicity

--- Assets/script.js
+++ Assets/script.js
@@ -4,19 +4,11 @@ var passwordLowLetters = ["abcdefghijklmnopqrstuvwxyz"]
 var passwordUpLetters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
 var passwordNumbers = ["0123456789"]
 var passwordSpecialChar = ["!@#$%&*?,"]
-var answerValue1 = 0
-var answerValue2 = 0
-var answerValue3 = 0
-var answerValue4 = 0
-var finalValue = 0
-var finalArray = []
-var passwordNum1 = 0
+var passwordArray = []
+var answerValue = 0
+var inputNum = 0
 var ranNum = 0
 var arrayGen = []
-
-
-// 26 + 26 +10 + 9 = 71
-
 
 // Write password to the #password input
 function writePassword() {
@@ -35,121 +27,77 @@ function passwordPrompt(){
 
   var passwordNum = prompt("How many characters do you want your password to be?", "Please input a number between 8-128");
   
-  passwordNum1 = parseInt(passwordNum);
+  inputNum = parseInt(passwordNum);
 
-  if (passwordNum1 >= 8 && passwordNum1 <= 128) {
-    Prompt5()
+  if (inputNum >= 8 && inputNum <= 128) {
+    Prompt()
   }
-  else if (passwordNum1 < 8 || passwordNum1 > 128) {
+  else if (inputNum < 8 || inputNum > 128) {
     alert("Please input a number between 8-128")
     passwordPrompt()
     }
     }  
     
     
-    function Prompt5() {
+    function Prompt() {
     
       var passwordLow = confirm("Do you want lowercase letters?")
       if (passwordLow===true){
-        var passwordArray1 = passwordLowLetters
-        var answerValue1 =  26
+        passwordArray = passwordArray + passwordLowLetters
+        answerValue =  26
         
       }
-      else if (passwordLow===false) {
-        var passwordArray1 = []
-        var answerValue1 = 0
-      }
+      
 
 
 
     var passwordUp = confirm("Do you want uppercase letters letters?")
       if (passwordUp===true){
-      var passwordArray2 =passwordUpLetters
-      var answerValue2 = 26
+      passwordArray = passwordArray + passwordUpLetters
+      answerValue = answerValue + 26
      
     }
-    else if (passwordUp===false) {
-      var passwordArray2 = []
-      var answerValue2 = 0
-    }
-
-
+   
 
     var passwordNumb = confirm("Do you want numbers?")
     if (passwordNumb===true){
-      var passwordArray3 =passwordNumbers
-      var answerValue3 =  10
+      passwordArray = passwordArray + passwordNumbers
+      answerValue =  answerValue + 10
       
     }
-    else if (passwordNumb===false) {
-      var passwordArray3 = []
-      var answerValue3 = 0
-    }
-
+   
 
     var passwordSpec = confirm("Do you want special characters?")
     if (passwordSpec===true){
-      var passwordArray4 = passwordSpecialChar
-      var answerValue4 = 9
+      passwordArray = passwordArray + passwordSpecialChar
+      answerValue = answerValue + 9
      
     }
-    else if (passwordSpec===false) {
-      var passwordArray4 = []
-      var answerValue4 = 0
-    }
+   
    
   
-  finalValue = answerValue1+answerValue2+answerValue3+answerValue4-1
-  finalArray = passwordArray1 + passwordArray2 + passwordArray3 + passwordArray4
- 
+  answerValue = answerValue-1
+   
   
-if (finalValue === -1) {
+if (answerValue === -1) {
   alert("Must click OK on one of the password options!")
   passwordPrompt()  
 }   
-else if (finalValue != -1)
-console.log(finalArray.length)  
+else if (answerValue != -1)
+ 
 finalPassword()
 }
 
 function finalPassword() {
   
-  for (arrayGen.length=0; arrayGen.length< passwordNum1; arrayGen.length++) {
+  for (arrayGen.length=0; arrayGen.length< inputNum; arrayGen.length++) {
   
   min = Math.ceil(0);
-  max = Math.floor(finalValue);
+  max = Math.floor(answerValue);
   ranNum =  Math.floor(Math.random() * (max - min) + min);
-  console.log(ranNum)
-  arrayGen = arrayGen + finalArray[ranNum]
+  arrayGen = arrayGen + passwordArray[ranNum]
   console.log(arrayGen)
   }
 
 }
   
-
-
-
-
-
-
-// create a funtion to take in all the password criteria
-
-  // prompt user for lenght of password and save as a variable
-  // check to make sure the lenght is between 8 and 128 characters
-  // confirm lowercase, uppercase, numberic and special character
-  // validate one of the 4 options were selected
-  // create an object to store the user input
-
-// create a separate global array for each of the 4 confirmations
-
-//inside passwordPrompt function
-  //create a variable to store user input from the password option function
-  //create a new array that will take in all possible charcters that an be used 
-  //for my new password
-  //create conditional statements to push into possible character array....
-  //ie if the user selected lower case then we need to push those elements in
-  //the lowercase array into pssible character array
-  //loop through the possible character array and retrun
-  //characters based on the lenght given by user
-  //return the result and pass to be generated on the page
-
